9e32191c0f2e387d0fea8ddf0ccb489a7cb5abce
Ashley Tolen <tolenashley7@gmail.com>
2019-05-16 15:31:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  badges
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |name, room|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!" # is there a better way to do with w.o. + 1?
+  end
+  room_assignments
+end
+
+def printer
+  batch_badge_creator.collect do |badges|
+    puts badges
+  end
+  
+  assign_rooms.collect do |room_assignments|
+    puts room_assignments
+  end
+end
